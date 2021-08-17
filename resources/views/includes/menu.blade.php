4250d65490e5b44d3bcd7b0c445b423698f9fb15
jonasdamher <jonas.damher@gmail.com>
2021-08-17 08:40:58 +0100

    UPDATE: change styles, add brand

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,24 +1,32 @@
-    <div class="navbar-light shadow-sm menu">
-        <div class="p-3 pt-4">
-            <ul class="navbar-nav d-flex flex-column">
+    <div class="navbar-light menu border-right">
+        <div class="d-flex flex-column">
+            <div class="py-3 px-4 border-bottom brand d-flex justify-content-center">
+                <a class="navbar-brand" href="#" title="squadsy">.squadsy</a>
+            </div>
+            <ul class="navbar-nav d-flex flex-column py-3 px-4 ">
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><span class="icon pr-2"><i class="fas fa-file"></i></span>Incidencias</a>
+                    <a class="nav-link" href="#"><span class="icon pr-2"><i
+                                class="fas fa-file"></i></span>Incidencias</a>
                 </li>
                 <li class="nav-item active">
                     <a class="nav-link" href="#"><span class="icon pr-2"><i class="fas fa-box"></i></span>Artículos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><span class="icon pr-2"><i class="fas fa-industry"></i></span>Proveedores</a>
+                    <a class="nav-link" href="#"><span class="icon pr-2"><i
+                                class="fas fa-industry"></i></span>Proveedores</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><span class="icon pr-2"><i class="fas fa-store-alt"></i></span>Empresas</a>
+                    <a class="nav-link" href="#"><span class="icon pr-2"><i
+                                class="fas fa-store-alt"></i></span>Empresas</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#"><span class="icon pr-2"><i class="fas fa-users"></i></span>Usuarios</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><span class="icon pr-2"><i class="fas fa-cogs"></i></span>Configuración</a>
+                    <a class="nav-link" href="#"><span class="icon pr-2"><i
+                                class="fas fa-cogs"></i></span>Configuración</a>
                 </li>
             </ul>
         </div>
     </div>
+    <div class="bg-menu"></div>
